311ad1528eea8e470d2d95c398d44819138b9437
Wesley S <wexxcode@gmail.com>
2020-12-04 18:34:14 -0300

    Adicionando imagem

--- src/components/Content/styles.ts
+++ src/components/Content/styles.ts
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
-import bg from '../../assets/AJI-100-C_native_1600.jpg'
+import bg from '../../assets/gdn-yellow.jpeg';
 
 export const Container = styled.div`
     grid-area: CT;
     background: #eaeaea url(${bg});
-        background-size: cover;
-    background-position: 50% 50%;
+    background-size: cover;
+    background-position: 50% 75%;
     width: 100%;
     height: 100%;
 `;
